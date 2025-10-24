6dcc13cfdd9900480278f36274b64e3b44a9d57b
antonios <antonio.stiube@gmail.com>
2025-10-24 20:59:47 +0300

    removed the .env

--- public/firebaseauth.js
+++ public/firebaseauth.js
@@ -11,15 +11,7 @@ import {
     doc 
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+import { firebaseConfig } from '../../firebaseConfig.js';
 
 
 // Initialize Firebase FIRST
--- public/homepage.js
+++ public/homepage.js
@@ -8,14 +8,8 @@ import {
   getFirestore,
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+import { firebaseConfig } from '../../firebaseConfig.js';
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
